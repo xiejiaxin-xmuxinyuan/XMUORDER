3d4d0c859e063438cdeddc3c57a316632f28712d
kgg-moon <1559144949@qq.com>
2021-09-30 09:30:12 +0800

    增加跳转信息完善以及跳转食堂页面的功能

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,7 +3,6 @@ const app = getApp()
 const db = wx.cloud.database()
 var that
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -14,14 +13,13 @@ Page({
     address: null,
     canteen: [],
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     that = this
     wx.showLoading({
-      title: '获取活动信息中',
+      title: '获取信息中',
     })
     db.collection("canteen")
       .get()
@@ -40,10 +38,44 @@ Page({
 
   },
   onNavChange:function(e){
+    if (app.globalData.isActive){
+      const pageCurr = e.currentTarget.dataset.cur
+      if (e.currentTarget.dataset.cur === 'user') {
+        const {name, address,phone} = app.globalData
+        that.setData({name,address, pageCurr,phone})
+      } else {
+        that.setData({pageCurr})
+      }
+    }
+    else {
+      that.goToInform()
+    }
+
+  },
+  toOrder:function(e){
     console.log(e)
-    var pageChange = e.currentTarget.dataset.cur
-    that.setData({
-      pageCurr : pageChange
+    if(!app.globalData.isActive){
+      that.goToInform()
+    }else{
+      app.globalData.canteen=e.currentTarget.dataset.canteen
+      wx.navigateTo({
+        url: '../canteen/canteen',
+      })
+    }
+  },
+  goToInform:function(){
+    wx.showModal({
+      title: '请完善信息',
+      showCancel: true,
+      success: val => {
+        // 用户点击确认
+        if (val.confirm) {
+          wx.navigateTo({
+            url: '../infoForm/infoForm',
+          })
+        }
+      },
+      fail: err => console.error(err)
     })
   }
 })
