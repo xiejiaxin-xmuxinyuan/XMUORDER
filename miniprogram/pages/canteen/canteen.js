f7467dbdcbd6406d6ebc9cc8b9cd447834e0cc5e
kgg-moon <1559144949@qq.com>
2021-10-01 00:40:28 +0800

    初步设计食堂页面

--- miniprogram/pages/canteen/canteen.js
+++ miniprogram/pages/canteen/canteen.js
@@ -3,12 +3,19 @@ var that
 const db = wx.cloud.database()
 const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    canteen:{}
+    canteen:{},
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    Custom: app.globalData.Custom,
+    TabCur: 0,
+    MainCur: 0,
+    VerticalNavTop: 0,
+    list: [],
+    load: true
   },
 
   /**
@@ -16,7 +23,13 @@ Page({
    */
   onLoad: function (options) {
     that = this
+    var list = app.globalData.canteen.orderTypeList
+    for (let i = 0; i < list.length; i++) {
+      list[i].id = i;
+    }
     that.setData({
+      list: list,
+      listCur: list[0],
       canteen : app.globalData.canteen
     })
   },
@@ -27,5 +40,42 @@ Page({
   onShow: function () {
 
   },
-
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      MainCur: e.currentTarget.dataset.id,
+      VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
+    })
+  },
+  VerticalMain(e) {
+    let that = this;
+    let list = this.data.list;
+    let tabHeight = 0;
+    if (this.data.load) {
+      for (let i = 0; i < list.length; i++) {
+        let view = wx.createSelectorQuery().select("#main-" + list[i].id);
+        view.fields({
+          size: true
+        }, data => {
+          list[i].top = tabHeight;
+          tabHeight = tabHeight + data.height;
+          list[i].bottom = tabHeight;     
+        }).exec();
+      }
+      that.setData({
+        load: false,
+        list: list
+      })
+    }
+    let scrollTop = e.detail.scrollTop + 20;
+    for (let i = 0; i < list.length; i++) {
+      if (scrollTop > list[i].top && scrollTop < list[i].bottom) {
+        that.setData({
+          VerticalNavTop: (list[i].id - 1) * 50,
+          TabCur: list[i].id
+        })
+        return false
+      }
+    }
+  }
 })
