40907e0d822236b7cc23716d842939ce51c3b74f
如初 <1003019131@qq.com>
2021-10-02 17:01:12 +0800

    新增公告滑动切换类型

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -13,7 +13,7 @@ Page({
     address: null,
     canteen: [],
     notices: [],
-    noticeType: ['公共', '翔安', '思明', '海韵'],
+    noticeTypes: ['公共', '翔安', '思明', '海韵'],
     noticeCurrType: "公共",
     identity: null
   },
@@ -132,6 +132,33 @@ Page({
     wx.navigateTo({
       url: '../admin/admin',
     })
+  },
+  infoTouchStart: function (e) {
+    that.startPageX = e.changedTouches[0].pageX;
+  },
+  infoTouchEnd: function (e) {
+    const moveX = e.changedTouches[0].pageX - that.startPageX;
+    if (Math.abs(moveX) >= 150) {
+      let noticeCurrType = that.data.noticeCurrType
+      let noticeTypes = that.data.noticeTypes
+      let index = -1
+      for (let i = 0; i < noticeTypes.length; i++) {
+        if (noticeTypes[i] === noticeCurrType) {
+          index = i
+          break
+        }
+      }
+      let newIndex = index
+      let maxPage = that.data.noticeTypes.length - 1;
+      if (moveX > 0) {
+        newIndex = index === maxPage ? 0 : index + 1
+      } else {
+        newIndex = index === 0 ? maxPage : index - 1
+      }
+      that.setData({
+        noticeCurrType: that.data.noticeTypes[newIndex]
+      })
+    }
   }
 
 })
