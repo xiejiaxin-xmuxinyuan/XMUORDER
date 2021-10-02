2361e15bef6a97ac418c5f14d80b28f88d76f779
如初 <1003019131@qq.com>
2021-10-03 00:37:33 +0800

    修复合并冲突，新增欢迎页、管理页

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,65 +1,37 @@
-// pages/index/index.js
+// pages/welcome/welcome.js
+var that
+var app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    identity: 'staff'
+    // identity: null
+    // 管理员admin 员工staff？
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    that = this
+    //globalData问题待解决
+    // console.log(that.data.identity)
+    // that.setData({
+    //   identity: app.globalData.identity
+    // })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  redictoUser: function(e){
+    wx.redirectTo({
+      url: '../../subpackages/order/pages/index/index',
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  redictoAdmin: function(e){
+    wx.redirectTo({
+      url: '../../subpackages/admin/pages/index/index',
+    })
   }
 })
--- miniprogram/subpackages/admin/pages/index/index.js
+++ miniprogram/subpackages/admin/pages/index/index.js
@@ -1,66 +1,60 @@
-// subpackages/admin/pages/index/index.js
+// pages/index/index.js
+const app = getApp()
+var that
+
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    pageCurr: "admin",
+    name: "测试姓名",
+    phone: "123123123",
+    identity: "admin",
+    //管理员、员工（staff）
 
+
+    //可以考虑在里面加上页面路径键值对
+    iconList: [{
+      icon: 'noticefill',
+      color: 'yellow',
+      name: '公告'
+    }, {
+      icon: 'friendfill',
+      color: 'grey',
+      name: '员工管理'
+    }, {
+      icon: 'taoxiaopu',
+      color: 'blue',
+      name: '商店信息'
+    }, {
+      icon: 'goodsnewfill',
+      color: 'orange',
+      name: '商品管理'
+    }, {
+      icon: 'rankfill',
+      color: 'red',
+      name: '统计'
+    }, {
+      icon: 'settingsfill',
+      color: 'olive',
+      name: '设置'
+    }, {
+      icon: 'questionfill',
+      color: 'black',
+      name: '帮助'
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    that = this
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onNavChange: function (e) {
+    const pageCurr = e.currentTarget.dataset.cur
+    that.setData({
+      pageCurr
+    })
   }
+
 })
