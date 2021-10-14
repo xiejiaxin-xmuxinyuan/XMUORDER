010a157b70b65a24fadc2819f78c597c13b32560
kgg-moon <1559144949@qq.com>
2021-10-14 19:57:09 +0800

    增加对营业时间的判断

--- miniprogram/subpackages/order/pages/index/index.js
+++ miniprogram/subpackages/order/pages/index/index.js
@@ -81,10 +81,20 @@ Page({
     if (!app.globalData.isActive) {
       that.goToInform()
     } else {
+      var myDate = new Date()
+      var myTime = this.formatDate(myDate)
       app.globalData.canteen = e.currentTarget.dataset.canteen
-      wx.navigateTo({
-        url: '../canteen/canteen',
-      })
+      var endTime = e.currentTarget.dataset.canteen.endTime
+      if(endTime < myTime){
+        wx.showToast({
+          title: '不在营业时间',
+          icon:'error'
+        })
+      }else{
+        wx.navigateTo({
+          url: '../canteen/canteen',
+        })
+      }
     }
   },
   goToInform: function () {
@@ -159,10 +169,24 @@ Page({
     res.data.sort(userNoticesSort)
     return res.data //返回排序后数据
   },
-  toRecord:function(e){
+  toRecord: function (e) {
     wx.navigateTo({
       url: '../record/record',
     })
+  },
+  formatDate: function (inputTime) { //该函数用于格式化时间戳
+    var date = new Date(inputTime);
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    m = m < 10 ? ('0' + m) : m;
+    var d = date.getDate();
+    d = d < 10 ? ('0' + d) : d;
+    var h = date.getHours();
+    h = h < 10 ? ('0' + h) : h;
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    minute = minute < 10 ? ('0' + minute) : minute;
+    second = second < 10 ? ('0' + second) : second;
+    return h + ':' + minute;
   }
-
 })
