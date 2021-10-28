3fa02c3d986c682709099bcbb86f257972220f18
kgg-moon <1559144949@qq.com>
2021-10-28 09:19:06 +0800

    反馈提交云函数

---
+++ cloudfunctions/submitFeedback/index.js
@@ -0,0 +1,33 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+const db = cloud.database()
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  const record = event.record
+  const feedback = event.feedback
+  try {
+    const openid = wxContext.openid
+    const rID = record._id
+    await db.collection('userFeedbacks').add({
+      data: {
+        record: record,
+        openid: openid,
+        feedback: feedback,
+        rID : rID,
+        state : 0
+      }
+    })
+    return {
+      success: true
+    }
+  } catch (e) {
+    return {
+      success: false
+    }
+  }
+
+}
