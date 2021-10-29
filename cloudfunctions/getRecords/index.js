427a746e03d5a5aafb3d9c77279cd684892a391f
如初 <1003019131@qq.com>
2021-10-29 23:12:07 +0800

    我的订单分页加载

--- cloudfunctions/getRecords/index.js
+++ cloudfunctions/getRecords/index.js
@@ -9,40 +9,76 @@ const _ = db.command
 const $ = _.aggregate
 
 exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  const openid = wxContext.OPENID
+
+  const pageSize = "pageSize" in event ? event.pageSize : 5 // 每页数据量
+  const currPage = "currPage" in event ? event.currPage : 1 //查询的当前页数
+
+
   return new Promise((resolve, reject) => {
-    const wxContext = cloud.getWXContext()
-    const openid = wxContext.OPENID
-    db.collection('userRecord').aggregate()
-      .match({
+    db.collection('userRecord')
+      .where({
         openid: openid
       })
-      .lookup({
-        let: {
-          id: '$_id'
-        },
-        from: 'userFeedbacks',
-        pipeline: $.pipeline()
-          .match(_.expr($.and([ //匹配openid和rID
-            $.eq(['$_openid', openid]),
-            $.eq(['$rID', '$$id'])
-          ])))
-          .replaceRoot({ //只显示state
-            newRoot: {
-              state: '$state'
-            }
-          })
-          .done(),
-        as: 'feedback' //临时位置
-      })
-      .sort({ //日期字符串从大到小排序
-        date: -1
-      })
-      .end()
+      .count()
       .then(res => {
-        resolve({
-          success: true,
-          record: res.list
-        })
+        const totalCount = res.total
+        const totalPage = totalCount === 0 ? 0 : totalCount <= pageSize ? 1 : Math.ceil(totalCount / pageSize)
+
+        if (currPage > totalPage) {
+          resolve({
+            success: true,
+            record: [],
+            currPage: currPage,
+            totalPage: totalPage,
+            totalCount: totalCount,
+          })
+        }
+
+        db.collection('userRecord').aggregate()
+          .match({
+            openid: openid
+          })
+          .sort({ //日期字符串从大到小排序
+            date: -1
+          })
+          .skip((currPage - 1) * pageSize)
+          .limit(pageSize)
+          .lookup({
+            let: {
+              id: '$_id'
+            },
+            from: 'userFeedbacks',
+            pipeline: $.pipeline()
+              .match(_.expr($.and([ //匹配openid和rID
+                $.eq(['$_openid', openid]),
+                $.eq(['$rID', '$$id'])
+              ])))
+              .replaceRoot({ //只显示state
+                newRoot: {
+                  state: '$state'
+                }
+              })
+              .done(),
+            as: 'feedback' //临时位置
+          })
+          .end()
+          .then(res => {
+            resolve({
+              success: true,
+              record: res.list,
+              currPage: currPage,
+              totalPage: totalPage,
+              totalCount: totalCount,
+            })
+          })
+          .catch(e => {
+            console.error(e)
+            reject({
+              success: false
+            })
+          })
       })
       .catch(e => {
         console.error(e)
@@ -50,5 +86,7 @@ exports.main = async (event, context) => {
           success: false
         })
       })
+
+
   })
 }
--- miniprogram/subpackages/order/pages/record/record.js
+++ miniprogram/subpackages/order/pages/record/record.js
@@ -6,46 +6,76 @@ Page({
    * 页面的初始数据
    */
   data: {
-    record: []
+    record: [],
+    currPage: 0,
+    totalPage: 1
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     that = this
-    util.showLoading('加载中')
-    wx.cloud.callFunction({
-        name: 'getRecords',
-      }).then(res => {
-        util.hideLoading()
-        if (res.result.success) {
+    that.getRecords(1) //第一页
+      .then(res => {
+        that.setData({
+          record: res.record,
+          currPage: res.currPage,
+          totalPage: res.totalPage,
+        })
+      })
+  },
+  onReachBottom() {
+    let currPage = that.data.currPage
+    let totalPage = that.data.totalPage
+    if (currPage < totalPage) {
+      that.getRecords(currPage + 1)
+        .then(res => {
+          let record = that.data.record
           that.setData({
-            record: res.result.record
+            record: record.concat(res.record),
+            currPage: res.currPage,
+            totalPage: res.totalPage,
           })
-        } else {
-          util.showToast('加载失败', 'error')
-          setTimeout(() => {
-            wx.navigateBack({
-              delta: 1,
-            })
-          }, 1000);
-        }
-      })
-      .catch(e => {
-        util.hideLoading()
-        util.showToast('加载失败', 'error')
-        setTimeout(() => {
-          wx.navigateBack({
-            delta: 1,
-          })
-        }, 1000);
-      })
+        })
+    }
   },
   feedback: function (e) {
     var record = that.data.record[e.currentTarget.dataset.index]
     record = JSON.stringify(record)
     wx.navigateTo({
       url: '../feedback/feedback?record=' + record,
+    })
+  },
+  getRecords: currPage => {
+    return new Promise((resolve, reject) => {
+      util.showLoading('加载中')
+      wx.cloud.callFunction({
+          name: 'getRecords',
+          data: {
+            currPage: currPage
+          }
+        }).then(res => {
+          util.hideLoading()
+          if (res.result.success) {
+            resolve(res.result)
+          } else {
+            util.showToast('加载失败', 'error')
+            setTimeout(() => {
+              wx.navigateBack({
+                delta: 1,
+              })
+            }, 1000);
+          }
+        })
+        .catch(e => {
+          util.hideLoading()
+          util.showToast('加载失败', 'error')
+          setTimeout(() => {
+            wx.navigateBack({
+              delta: 1,
+            })
+          }, 1000);
+        })
     })
   }
 })
