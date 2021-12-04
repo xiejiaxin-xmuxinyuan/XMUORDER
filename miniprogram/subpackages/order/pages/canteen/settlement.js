0f0cc7003f0f9cbf5a65eaa5d595cc5b07e1379b
如初 <1003019131@qq.com>
2021-12-04 15:42:17 +0800

    完善下单功能

--- miniprogram/subpackages/order/pages/canteen/settlement.js
+++ miniprogram/subpackages/order/pages/canteen/settlement.js
@@ -66,60 +66,195 @@ Page({
     const orderList = that.data.orderList
     const foodList = that.data.foodList
     const canteen = that.data.canteen
+    const user = that.data.user
+
     if (!that.data.isTimePick) {
       util.showToast('时间未选择', 'error')
     } else {
-      var userRecord = {
-        cID: canteen.cID,
-        cName: canteen.name,
-        record: [],
-        allPrice: that.data.money
+      //构造订单数据
+      const goodsPrice = that.data.money * 100
+      const otherFee = 0 //打包费等等（单位分）
+      const discount = 0 //优惠
+
+      var orderInfo = {
+        userInfo: { //用户信息
+          openid: undefined,
+          name: user.name,
+          phone: user.phone
+        },
+        payInfo: { //支付信息
+          feeInfo: { //金额信息
+            goodsPrice: goodsPrice,
+            otherFee: otherFee,
+            discount: discount,
+            totalFee: goodsPrice + otherFee, //不包括优惠的总费用
+            cashFee: goodsPrice + otherFee - discount //实付款
+          },
+          tradeState: "NOTPAY" //具体见查询订单接口文档
+        },
+        goodsInfo: { //商品信息
+          shopInfo: {
+            cID: canteen.cID,
+            name: canteen.name,
+            subMchId: '1616983338'
+          },
+          record: []
+        },
+        getFoodInfo: {
+          time: that.data.timeToPick[that.data.pickedIndex],
+          place: canteen.name + "默认取餐点",
+          state: 'NOTGET' //未取餐
+        },
+        orderInfo: {
+          timeInfo: {
+            createTime: undefined,
+            payTime: undefined,
+            receiveTime: undefined,
+            endTime: undefined
+          },
+          outTradeNo: undefined,
+          orderState: 'NOTPAY' //未支付
+        }
       }
+
+      //填充商品信息中的商品记录 orderInfo.goodsInfo.record
       for (const key in orderList) {
-        if (key === 'length') {
+        if (key === 'length' || key=== 'delete') {
           continue
         }
-        let index1 = orderList[key][1]
-        let index2 = orderList[key][2]
-
-        let food = foodList[index1].food[index2]
+        let food = foodList[orderList[key][1]].food[orderList[key][2]]
         let foodRecord = {
           food: food.name,
           num: food.orderNum,
-          price: food.price * food.orderNum
+          price: food.price
         }
-        userRecord.record.push(foodRecord)
+        orderInfo.goodsInfo.record.push(foodRecord)
       }
 
+      //请求下单
       wx.showLoading({
         title: '提交订单中',
         mask: true
       })
-      // TODO: 添加到商家订单
-
-      // 添加到用户记录
       wx.cloud.callFunction({
-        name: 'settlementSubmit',
-        data: {
-          userRecord: userRecord
-        }
-      }).then(res => {
-        wx.hideLoading()
-        if (!res.result.success) {
-          util.showToast('提交失败', 'error')
-        } else {
-          //在canteen页面onUpload中清空当前餐厅购物车
+          name: 'payOrder',
+          data: {
+            orderInfo: orderInfo,
+            order: {
+              subMchId: '1616983338',
+              body: canteen.name + "-点餐",
+              totalFee: orderInfo.payInfo.feeInfo.totalFee
+            }
+          }
+        })
+        .then(payOrderRes => {
+          wx.hideLoading()
+          if (!payOrderRes.result.success) { //下单失败
+            util.showToast('订单提交失败', 'error')
+            return
+          }
+
+          //下单成功
+          //返回退出canteen页面后会清空当前餐厅购物车
           app.globalData.allOrderList[canteen.cID].delete = true
 
-          util.showToast('下单成功', 'success')
-          setTimeout(() => {
-            wx.navigateBack({
-              delta: 2,
+          //发起支付
+          util.showLoading('正在支付')
+          wx.requestPayment({
+              ...payOrderRes.result.payment, //  ...是展开符
             })
-          }, 1000);
-        }
-      })
+            .then(res => { //支付成功
+              util.showLoading('核实订单中')
+
+              //云函数查单
+              const outTradeNo = payOrderRes.result.outTradeNo
+              let nonceStr = payOrderRes.result.payment.nonceStr
+              that.orderQuery('1616983338', outTradeNo, nonceStr)
+                .then(() => { //进入支付成功页面
+                  setTimeout(() => {
+                    wx.navigateTo({
+                      url: '../order/paySuccess',
+                    })
+                  }, 1000)
+                })
+                .catch(() => { //进入订单支付详情页面
+                  setTimeout(() => {
+                    wx.navigateTo({
+                      url: '../order/payOrder',
+                    })
+                  }, 1000)
+                })
+            })
+            .catch(err => { //支付出错
+              if (err.errMsg !== 'requestPayment:fail cancel') { //非取消支付报错则进行查单
+                util.showLoading('核实订单中')
+                //云函数查单
+                const outTradeNo = payOrderRes.result.outTradeNo
+                let nonceStr = payOrderRes.result.payment.nonceStr
+                that.orderQuery('1616983338', outTradeNo, nonceStr)
+                  .then(() => { //进入支付成功页面
+                    setTimeout(() => {
+                      wx.navigateTo({
+                        url: '../order/paySuccess',
+                      })
+                    }, 1000)
+                  })
+                  .catch(() => { //进入订单支付详情页面
+                    setTimeout(() => {
+                      wx.navigateTo({
+                        url: '../order/payOrder',
+                      })
+                    }, 1000)
+                  })
+              } else {
+                wx.hideLoading()
+                util.showToast('订单未支付', 'error')
+                setTimeout(() => {  //进入订单支付详情页面
+                  wx.navigateTo({
+                    url: '../order/payOrder',
+                  })
+                }, 1000)
+              }
+            })
+        })
+        .catch(err => {
+          wx.hideLoading()
+          util.showToast('订单提交失败', 'error')
+        })
     }
+  },
+  orderQuery: function (sub_mch_id, out_trade_no, nonce_str) {
+    return new Promise((resolve, reject) => {
+      wx.cloud.callFunction({
+          name: 'payOrderQuery',
+          data: {
+            sub_mch_id: '1616983338',
+            out_trade_no: payOrderRes.result.outTradeNo,
+            nonce_str: payOrderRes.result.payment.nonceStr //借用统一下单的随机字符串
+          }
+        })
+        .then(res => {
+          wx.hideLoading()
+          if (res.result.success) {
+            let resInfo = res.result.info
+            if (resInfo.tradeState === 'SUCCESS') { //结果是支付成功
+              util.showToast('支付成功', 'success')
+              resolve() //表明进入支付成功页面
+            } else {
+              util.showToast('订单未支付')
+              reject() //表明进入订单支付详情页面
+            }
+          } else {
+            util.showToast('订单核实失败')
+            reject() //表明进入订单支付详情页面
+          }
+        })
+        .catch(e => {
+          wx.hideLoading()
+          util.showToast('订单核实失败')
+          reject() //表明进入订单支付详情页面
+        })
+    })
   }
 })
 
