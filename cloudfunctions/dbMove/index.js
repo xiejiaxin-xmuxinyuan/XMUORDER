bbb5f36ac1110703dac9e29d48e7e76efb2fcaff
如初 <1003019131@qq.com>
2021-12-08 17:16:32 +0800

    云函数附加移除表全部数据的功能

--- cloudfunctions/dbMove/index.js
+++ cloudfunctions/dbMove/index.js
@@ -1,8 +1,11 @@
 /**
- * 云函数聚合联表查询单个餐厅信息（从food表中获取食物信息）
+ * 云函数删除表的数据
+ * 若要清空某个表，请在云函数云端调试中带上removeAll参数触发此函数
+ * 
  * 参数： table: string 表名
  *        _id:  string 要删除记录的_id
- *                    
+ *        (选填)removeAll: '我确定要删除表中所有数据' 则删除整个表的所有数据
+ *
  * 返回： object 
  *        成功：{success: 1}
  *        失败：{success: 0}
@@ -15,6 +18,15 @@ cloud.init({
 const db = cloud.database()
 
 exports.main = async (event, context) => {
+  if (event.removeAll === '我确定要删除表中所有数据'){
+    const _ = db.command
+    return await db.collection(event.table)
+    .where({
+      _id: _.exists(true)
+    })
+    .remove()
+  }
+
   return new Promise((resolve, reject) => {
     db.collection(event.table).doc(event._id)
       .remove()
