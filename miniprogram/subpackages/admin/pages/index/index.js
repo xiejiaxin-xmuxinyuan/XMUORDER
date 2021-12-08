bbd370acdbb168e7708bc1b48f0a050c0461a888
如初 <1003019131@qq.com>
2021-12-08 21:17:19 +0800

    商户订单页面部分内容

--- miniprogram/subpackages/admin/pages/index/index.js
+++ miniprogram/subpackages/admin/pages/index/index.js
@@ -1,6 +1,7 @@
 // pages/index/index.js
 const app = getApp()
 const db = wx.cloud.database()
+const _ = db.command
 const util = require('../../../../utils/util.js')
 
 var that
@@ -20,6 +21,12 @@ Page({
   data: {
     pageCurr: "admin",
     watchOrderFlag: false,
+    user: {},
+    intCurTime: 0,
+    orders: {
+      newOrders: [],
+      finishedOrdersCount: 0
+    },
     iconList: [{
         icon: 'noticefill',
         color: 'yellow',
@@ -66,25 +73,76 @@ Page({
   },
 
   onLoad: function (options) {
-    that = this
     util.showLoading('获取信息中')
+    that = this
 
-    var p1 = db.collection("canteen").get()
-    var p2 = that.getUserNotices()
+    const identity = app.globalData.identity
+    //当前时间
+    let date = new Date()
+    let h = date.getHours().toString().padStart(2, '0')
+    let m = date.getMinutes().toString().padStart(2, '0')
+    var intCurTime = parseInt(h + m)
 
-    Promise.all([p1, p2]).then(res => {
-      wx.hideLoading()
-      that.setData({
+    that.setData({
+      user: {
         name: app.globalData.name,
         phone: app.globalData.phone,
         address: app.globalData.address,
-        identity: app.globalData.identity
-      })
+        identity,
+        intCurTime
+      }
+    })
+
+    var p1, p2, p3, p4
+    if (identity.type !== 'superAdmin') {
+      p1 = db.collection("canteen").where({
+        cID: identity.cID
+      }).get()
+    } else {
+      p1 = db.collection("canteen").get()
+    }
+
+    p2 = that.getUserNotices()
+
+    if (identity.type !== 'superAdmin') {
+      p3 = that.getFinishedOrdersCount()
+      p4 = that.getNewOrders()
+    }
+
+    Promise.all([p1, p2]).then(res => {
+
       //放入全局变量
-      let canteen = res[0].data //餐厅数据
-      let notices = res[1] //公告数据
-      app.globalData.canteen = canteen
+      var canteens = res[0].data //餐厅数据
+      var notices = res[1] //公告数据
+      app.globalData.canteen = canteens
       app.globalData.notices = notices
+
+      if (identity.type !== 'superAdmin') {
+        var canteen = canteens[0]
+        canteen.inBusiness = false
+        for (let i = 0; i < canteen.businessTime.length; i++) {
+          const time = canteen.businessTime[i];
+          if (intCurTime > parseInt(time[0]) && intCurTime < parseInt(time[1])) {
+            canteen.inBusiness = true
+            break
+          }
+        }
+        that.setData({
+          intCurTime,
+          canteen,
+          pageCurr: 'order'
+        })
+
+        Promise.all([p3, p4]).then(res => {
+          that.setData({
+            'orders.finishedOrdersCount': res[0],
+            'orders.newOrders': res[1]
+          })
+          wx.hideLoading()
+        })
+      } else {
+        wx.hideLoading()
+      }
     })
   },
   onNavChange: function (e) {
@@ -133,7 +191,7 @@ Page({
     if (watchOrderFlag) {
       util.showLoading('开启订单推送')
 
-      let cID = that.data.identity.cID
+      let cID = that.data.user.identity.cID
       watcher = db.collection('orders')
         .where({
           'goodsInfo.shopInfo.cID': cID, //所属餐厅（同时是数据库安全权限内容）
@@ -143,9 +201,9 @@ Page({
           onChange: function (snapshot) {
             if (snapshot.type === 'init') {
               wx.hideLoading()
-              util.showToast('订单推送已开启', 'success', 2000)
+              util.showToast('订单推送已开启', 'success', 1500)
               that.setData({
-                watchOrderFlag: watchOrderFlag
+                watchOrderFlag: true
               })
             } else {
               console.log(snapshot)
@@ -153,12 +211,60 @@ Page({
           },
           onError: function (err) {
             wx.hideLoading()
-            util.showToast('订单推送异常', 'error', 2000)
+            util.showToast('订单推送异常', 'error', 1500)
             that.setData({
               watchOrderFlag: false
             })
           }
         })
+    } else {
+      util.showLoading('关闭订单推送')
+      watcher.close().then(res => {
+        wx.hideLoading()
+        util.showToast('订单推送已关闭', 'success', 1500)
+        that.setData({
+          watchOrderFlag: false
+        })
+      }).catch(e => {
+        wx.hideLoading()
+      })
     }
+  },
+  getFinishedOrdersCount: function () {
+    const cID = that.data.user.identity.cID
+    return new Promise((resolve, reject) => {
+      db.collection('orders')
+        .where({
+          'goodsInfo.shopInfo.cID': cID, //所属餐厅（同时是数据库安全权限内容）
+          'orderInfo.orderState': 'SUCCESS',
+          'orderInfo.timeInfo.endTime': _.gte(getTodayDateTime())
+        }).count().then(res => {
+          resolve(res.total)
+        }).catch(e => {
+          reject(e)
+        })
+    })
+  },
+  getNewOrders: function(){
+    const cID = that.data.user.identity.cID
+    return new Promise((resolve, reject) => {
+      db.collection('orders')
+      .where({
+        'goodsInfo.shopInfo.cID': cID, //所属餐厅（同时是数据库安全权限内容）
+        'orderInfo.orderState': 'NOTCONFIRM',
+      }).get().then(res => {
+        resolve(res.data)
+      }).catch(e => {
+        reject(e)
+      })
+    })
   }
 })
+
+function getTodayDateTime() {
+  var date = new Date()
+  let year = date.getFullYear()
+  let month = (date.getMonth() + 1).toString().padStart(2, '0')
+  let day = date.getDate().toString().padStart(2, '0')
+  return year + month + day + '000000'
+}
