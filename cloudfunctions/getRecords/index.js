b9e428592329388135fbd38e983588e5da588ed3
如初 <1003019131@qq.com>
2021-12-25 13:45:43 +0800

    删除注释

--- cloudfunctions/getRecords/index.js
+++ cloudfunctions/getRecords/index.js
@@ -114,7 +114,6 @@ exports.main = async (event, context) => {
       })
     })
 
-
     return {
       success: true,
       record: orders,
@@ -122,79 +121,6 @@ exports.main = async (event, context) => {
       totalPage: totalPage,
       totalCount: totalCount,
     }
-
-
-    // // 读取订单记录
-    // const ordersRes = await db.collection('orders').where({
-    //     'userInfo.openid': openid
-    //   }).orderBy('orderInfo.timeInfo.createTime', 'desc')
-    //   .skip((currPage - 1) * pageSize).limit(pageSize).get()
-    // var orders = ordersRes.data
-
-    // //异步读取反馈、订单商品封面
-    // var foodIDs = []
-    // var outTradeNoList = []
-    // for (let index = 0; index < orders.length; index++) {
-    //   // 反馈outTradeNo整合
-    //   outTradeNoList.push(orders[index].orderInfo.outTradeNo)
-    //   // 食物_id整合
-    //   const record = orders[index].goodsInfo.record
-    //   record.forEach(food => {
-    //     foodIDs.push(food._id)
-    //   })
-    // }
-
-    // // 反馈状态获取
-    // var p0 = db.collection('userFeedbacks').where({
-    //   _openid: openid,
-    //   outTradeNo: _.in(outTradeNoList)
-    // }).field({
-    //   _id: false,
-    //   state: true,
-    //   outTradeNo: true
-    // }).get()
-
-    // //封面获取
-    // foodIDs = Array.from(new Set(foodIDs)) //_id去重
-    // var p1 = db.collection('food').where({
-    //   _id: _.in(foodIDs)
-    // }).field({
-    //   coverImg: true
-    // }).get()
-
-    // //等待异步请求
-    // const res = await Promise.all([p0, p1])
-
-    // //outTradeNo转反馈状态
-    // var noToFeedbackState = {}
-    // res[0].data.forEach(feedback => {
-    //   noToFeedbackState[feedback.outTradeNo] = feedback.state
-    // })
-
-    // //id转图片链接对象
-    // var idToImg = {}
-    // res[1].data.forEach(food => {
-    //   idToImg[food._id] = food.coverImg
-    // })
-
-    // orders.forEach(order => {
-    //   //填充order中反馈
-    //   if (order.orderInfo.outTradeNo in noToFeedbackState) {
-    //     order.feedback = {
-    //       state: noToFeedbackState[order.orderInfo.outTradeNo]
-    //     }
-    //   }
-    //   // 填充order中food对应的封面链接
-    //   order.goodsInfo.record.forEach(food => {
-    //     if (food._id in idToImg) {
-    //       food.img = idToImg[food._id]
-    //     }else{
-    //       //默认图片
-    //       food.img = 'cloud://cloud1-4g4b6j139b4e50e0.636c-cloud1-4g4b6j139b4e50e0-1307666009/noImg.svg'
-    //     }
-    //   })
-    // })
-
   } catch (e) {
     console.error(e)
     return {
