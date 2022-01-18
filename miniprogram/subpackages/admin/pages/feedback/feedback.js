f37b77216acd1c85c6a279132c4686223aab3605
Kgg <74181372+Kgg-moon@users.noreply.github.com>
2022-01-18 10:02:45 +0800

    反馈查看分页

--- miniprogram/subpackages/admin/pages/feedback/feedback.js
+++ miniprogram/subpackages/admin/pages/feedback/feedback.js
@@ -2,6 +2,7 @@
 var that
 const app = getApp()
 const db = wx.cloud.database()
+const util = require('../../../../utils/util.js')
 Page({
 
   /**
@@ -9,31 +10,105 @@ Page({
    */
   data: {
     feedbacks: [],
-    stateTypes: ['已处理','未处理'],
-    stateCurr: '未处理'
+    stateTypes: ['已处理', '未处理'],
+    stateCurr: '未处理',
+    feedbackCurrPage: 1,
+    feedbackTotalCount: 0,
+    feedbackTotalPage: 0
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     that = this
+  },
+  onShow: function (options) {
     var cID = app.globalData.identity.cID
     db.collection('userFeedbacks').where({
         cID: cID
       })
-      .orderBy('state','desc')
-      .orderBy('date','asc')
+      .orderBy('state', 'desc')
+      .orderBy('date', 'asc')
       .get()
       .then(res => {
         that.setData({
           feedbacks: res.data
         })
       })
+    util.showLoading('加载中')
+    that.getFeedbacks(that.data.feedbackCurrPage, cID).then(() => {
+      util.hideLoading()
+    })
   },
-  stateTypeSelect:function(e){
+  getFeedbacks: function (feedbackCurrPage = 1, cID, pageSize = 10, ) {
+    return new Promise(async (resolve, reject) => {
+      if (feedbackCurrPage < 1) {
+        feedbackCurrPage = 1
+      }
+      const countResult = await db.collection('userFeedbacks').where({
+        cID: cID
+      }).count()
+      const feedbackTotalCount = countResult.total
+      const feedbackTotalPage = feedbackTotalCount === 0 ? 0 : feedbackTotalCount <= pageSize ? 1 : Math.ceil(feedbackTotalCount / pageSize)
+      if (feedbackTotalCount === 0) { //如果没有任何记录
+        that.setData({
+          feedbacks: [],
+          feedbackCurrPage: 1,
+          feedbackTotalPage: 0,
+          feedbackTotalCount: 0,
+        })
+        resolve()
+        return
+      }
+      if (feedbackCurrPage > feedbackTotalPage) {
+        feedbackCurrPage = feedbackTotalPage
+      }
+      db.collection('userFeedbacks')
+        .where({
+          cID: cID
+        })
+        .orderBy('state','desc')
+        .orderBy('date','asc')
+        .skip((feedbackCurrPage - 1) * pageSize).limit(pageSize)
+        .get().then(res => {
+          that.setData({
+            feedbacks: res.data,
+            feedbackCurrPage: feedbackCurrPage,
+            feedbackTotalPage: feedbackTotalPage,
+            feedbackTotalCount: feedbackTotalCount,
+          })
+          resolve()
+          return
+        })
+    })
+  },
+  feedbackChangePage: function (e) {
+    const currPage = that.data.feedbackCurrPage
+    const totalPage = that.data.feedbackTotalPage
+    if ('add' in e.currentTarget.dataset) { //增加
+      if (currPage <= totalPage - 1) {
+        util.showLoading('加载中')
+        that.getCanteens(currPage + 1).then(() => {
+          wx.hideLoading()
+        })
+      } else {
+        util.showToast('已经是最后一页啦')
+      }
+    } else { //减少
+      if (currPage > 1) {
+        util.showLoading('加载中')
+        that.getCanteens(currPage - 1).then(() => {
+          wx.hideLoading()
+        })
+      } else {
+        util.showToast('已经是第一页啦')
+      }
+    }
+  },
+  stateTypeSelect: function (e) {
     var stateType = e.currentTarget.dataset.name
     that.setData({
-      stateCurr : stateType
+      stateCurr: stateType
     })
   },
   dealFeedback: function (e) {
@@ -50,19 +125,19 @@ Page({
             util.showLoading('处理中')
             feedbacks[index].canteenFeedback = res.content
             feedbacks[index].state = 1
-            var feedback=feedbacks[index]
+            var feedback = feedbacks[index]
             wx.cloud.callFunction({
-              name : 'dbUpdate',
-              data : {
-                table : 'userFeedbacks',
-                _id : feedback._id,
-                formData :{
-                  'canteenFeedback' : feedback.canteenFeedback,
-                  'state' : feedback.state
+              name: 'dbUpdate',
+              data: {
+                table: 'userFeedbacks',
+                _id: feedback._id,
+                formData: {
+                  'canteenFeedback': feedback.canteenFeedback,
+                  'state': feedback.state
                 }
               },
-              success(res){
-                if(res.success){
+              success(res) {
+                if (res.success) {
                   util.hideLoading()
                   util.showToast('已接受')
                 }
@@ -80,16 +155,16 @@ Page({
           if (res.confirm) {
             util.showLoading('处理中')
             wx.cloud.callFunction({
-              name : 'payRefund',
-              data : {
+              name: 'payRefund',
+              data: {
                 outTradeNo: feedbacks[index].outTradeNo
               },
-              success(RES){
-                if(RES.success){
+              success(RES) {
+                if (RES.success) {
                   util.hideLoading()
                   feedbacks[index].canteenFeedback = '已退款',
-                  feedbacks[index].state = 1
-                  util.showToast('已退款','success')
+                    feedbacks[index].state = 1
+                  util.showToast('已退款', 'success')
                 }
               }
             })
@@ -102,22 +177,22 @@ Page({
               success(Res) {
                 if (Res.confirm) {
                   feedbacks[index].state = 1
-                 util.showLoading('处理中')
+                  util.showLoading('处理中')
                   var feedback = feedbacks[index]
                   wx.cloud.callFunction({
-                    name : 'dbUpdate',
-                    data : {
-                      table : 'userFeedbacks',
-                      _id : feedback._id,
-                      formData :{
-                        'canteenFeedback' : feedback.canteenFeedback,
-                        'state' : feedback.state
+                    name: 'dbUpdate',
+                    data: {
+                      table: 'userFeedbacks',
+                      _id: feedback._id,
+                      formData: {
+                        'canteenFeedback': feedback.canteenFeedback,
+                        'state': feedback.state
                       }
                     },
-                    success(res){
-                      if(res.success){
+                    success(res) {
+                      if (res.success) {
                         util.hideLoading()
-                        util.showToast('已处理','success')
+                        util.showToast('已处理', 'success')
                       }
                     }
                   })
@@ -132,7 +207,5 @@ Page({
       feedbacks: feedbacks
     })
   },
-  refund:function(){
-    wx.cloud
-  }
+
 })
